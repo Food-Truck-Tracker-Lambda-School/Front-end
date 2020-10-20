ae63a2725c0198f0aee862afe8a2de09f0632704
Phil <philip-lisak@lambdastudents.com>
2020-10-19 19:10:21 -0700

    Updated PrivateRoute to properly route user to dashboard on reg/login

--- food-truck-tracker/src/utils/PrivateRoute.js
+++ food-truck-tracker/src/utils/PrivateRoute.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { DinerDashboard } from '../components/diner/DinerDashboard';
+// import { OperatorProfileHome } from '../components/owner/OperatorProfileHome';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ roleId, ...rest }) => {
+	// Component = roleId === 1 ? DinerDashboard : OperatorProfileHome;
+
 	return (
 		<Route
 			{...rest}
-			render={() =>
-				localStorage.getItem('token') ? <Component /> : <Redirect to="/" />
+			render={(props) =>
+				localStorage.getItem('token') ? <Component {...props} /> : <Redirect to="/login" />
 			}
 		/>
 	);
+	
 };
 
 export default PrivateRoute;
