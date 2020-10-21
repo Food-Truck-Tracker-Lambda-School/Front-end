69f1d77252f5c137d9f184c99cf29f3c1b85eeb2
Allison Usher <allyjay317@gmail.com>
2020-10-20 19:16:51 -0700

    merge conflict

--- food-truck-tracker/src/utils/PrivateRoute.js
+++ food-truck-tracker/src/utils/PrivateRoute.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { DinerDashboard } from '../components/diner/DinerDashboard';
-import OperatorProfileHome  from '../components/operator/OperatorProfileHome';
+import OperatorProfileHome from '../components/operator/OperatorProfileHome';
 
 const PrivateRoute = ({ roleId, ...rest }) => {
-<<<<<<< HEAD
 	Component = roleId === 1 ? DinerDashboard : OperatorProfileHome;
-=======
-	Component = roleId === 1 ? DinerDashboard : /*OperatorProfileHome*/ DinerDashboard;
->>>>>>> 05ed04c3a5f7a4e5cb83b7a7e0bc0ab0c44dae48
 
 	return (
 		<Route
